68169cba89a67440e6eed11770e0aff20cf0e031
rogedev <apteruno@gmail.com>
2022-07-14 20:25:26 -0500

    add: select regionFilter

--- src/components/CountryList.svelte
+++ src/components/CountryList.svelte
@@ -1,6 +1,7 @@
 <script>
   import { onMount } from 'svelte'
   import CountryCard from './CountryCard.svelte'
+  import Select from 'svelte-select'
 
   export let countries
 
@@ -24,7 +25,8 @@
     })
   }
 
-  const filterByRegion = (region) => [...countriesList].filter((x) => x.region.toLowerCase() == region.toLowerCase())
+  const filterByRegion = (region) =>
+    [...countriesList].filter((x) => x.region.toLowerCase() == region.toLowerCase())
 
   const sortBy = (sortField, sortOrder) => {
     const arr = [...countriesList]
@@ -39,12 +41,8 @@
     return sortOrder == 'asc' ? arr : arr.reverse()
   }
 
-  const handleFilterClick = () => {
-    showFilter = !showFilter
-  }
-
-  const onChange = (event) => {
-    regionFilter = event.currentTarget.value
+  const onChangeSelect = (event) => {
+    regionFilter = event.detail.value
     applyFilters()
   }
 </script>
@@ -60,36 +58,13 @@
         on:keyup={applyFilters}
       />
     </div>
-    <!-- TODO fix dropdown -->
-    <div class="dropdownDiv">
-      <a href class="dropdownBtn" on:click={handleFilterClick}> Filter by Region </a>
-      {#if showFilter}
-        <ul class="dropdownUL">
-          {#each regions as region}
-            <li>
-              <input
-                class="dropdownInput"
-                type="radio"
-                bind:group={regionFilter}
-                on:change={onChange}
-              />
-              <p for={region}>{region}</p>
-            </li>
-          {/each}
-          <li>
-            <input
-              id="radioAll"
-              class="dropdownInput"
-              type="radio"
-              name="all"
-              value="All Regions"
-              checked={(regionFilter = '')}
-              on:change={onChange}
-            />
-            <p>All Regions</p>
-          </li>
-        </ul>
-      {/if}
+    <div class="region-select">
+      <Select
+        items={regions}
+        value={regionFilter}
+        on:select={onChangeSelect}
+        placeholder="Filter by Region"
+      />
     </div>
   </div>
 
@@ -137,57 +112,9 @@
     font-family: "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif";
     width: 420px;
   }
-  .dropdownBtn {
-    display: block;
-    background: #fff;
-    height: 50px;
-    width: 160px;
-    box-shadow: 1px 1px 7px 0px rgb(0, 0, 0, 0.1);
-    padding-right: 30px;
-    padding-left: 30px;
-    border-radius: 3px;
-    cursor: pointer;
-    font-weight: 600;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-  .dropdownBtn::after {
-    font-weight: 900;
-    content: '\f107';
-  }
-  .dropdownUL {
-    padding-left: 0;
-    text-align: left;
-    background-color: #fff;
-    margin-top: 3px;
-    padding: 10px 0;
-    box-shadow: 1px 1px 7px 0px rgb(0, 0, 0, 0.1);
-    border-radius: 3px;
-    position: absolute;
-    width: 220px;
-  }
-  .dropdownUL li {
-    list-style: none;
-    line-height: 2;
-    cursor: pointer;
-  }
-  .dropdownUL li p {
-    cursor: pointer;
-    padding: 0 26px;
-    display: block;
-    width: 148px;
-  }
-  .dropdownUL li:hover {
-    background-color: #f9f9f9;
-  }
-  .dropdownInput {
-    height: 1px;
-    clip: rect(1px, 1px, 1px, 1px);
-    position: absolute;
-  }
-  input[type='radio'] {
-    -webkit-appearance: radio;
+  .region-select {
+    position: relative;
+    width: 200px;
   }
 
   @media (max-width: 875px) {
@@ -201,9 +128,6 @@
     }
     .searchContainer {
       width: inherit;
-    }
-    .dropdownDiv {
-      margin-top: 40px;
     }
   }
 </style>
